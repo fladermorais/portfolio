8c873715d98d0b76494f439738a9c5d631605f44
Flademir <fladermorais@hotmail.com>
2022-04-28 20:05:16 -0300

    Removendo configuração de cor da tela inicial

--- resources/views/layouts/site/header.blade.php
+++ resources/views/layouts/site/header.blade.php
@@ -1,36 +1,9 @@
-  <!-- Start Switcher -->
-  <div class="demo_changer">
-    <div class="demo-icon"> <i class="fa fa-cog fa-spin fa-2x"></i> </div>
-    <!-- end opener icon -->
-    <div class="form_holder">
-        <div class="row">
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <div class="predefined_styles">
-                    <h4>Color Skins </h4>
-                    <!-- MODULE #3 --> 
-                    <a href="" rel="blue" class="styleswitch"><img src="{{ asset('Front/plugins/switcher/images/icons/pink.png') }}" alt="img"></a> <a href="" rel="purple" class="styleswitch"><img src="{{ asset('Front/plugins/switcher/images/icons/purple.png') }}" alt="img"></a> <a href="" rel="blue" class="styleswitch"><img src="{{ asset('Front/plugins/switcher/images/icons/blue.png') }}" alt="img"></a> <a href="" rel="green" class="styleswitch"><img src="{{ asset('Front/plugins/switcher/images/icons/green.png') }}" alt="img"></a> <a href="" rel="red" class="styleswitch"><img src="{{ asset('Front/plugins/switcher/images/icons/red.png') }}" alt="img"></a> <a href="" rel="yellow" class="styleswitch"><img src="{{ asset('Front/plugins/switcher/images/icons/yellow.png') }}" alt="img"></a> 
-                    <!-- END MODULE #3 --> 
-                    
-                </div>
-                <!-- end predefined_styles --> 
-            </div>
-            <!-- end col --> 
-            
-            <!-- end col --> 
-        </div>
-        <!-- end row --> 
-    </div>
-    <!-- end form_holder --> 
-</div>
-<!-- end demo_changer --> 
-<!-- End Switcher --> 
-
 <!-- Loader Landing Page -->
 <div id="ip-container" class="ip-container"> 
     <!-- initial header -->
     <header class="ip-header">
         <div class="col-lg-12 text-center">
-            <div class="ip-logo"><img  src="{{ asset('Front/img/loader-logo.png') }}" alt="logo"></div>
+            <div class="ip-logo"><img  src="{{ asset('storage/logo/' . config('app.empresas.logo')) }}" alt="logo"></div>
         </div>
         <div class="ip-loader"> <svg class="ip-inner" width="60px" height="60px" viewBox="0 0 80 80">
             <path class="ip-loader-circlebg" d="M40,10C57.351,10,71,23.649,71,40.5S57.351,71,40.5,71 S10,57.351,10,40.5S23.649,10,39.3,10z"/>
