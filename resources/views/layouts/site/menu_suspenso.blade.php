4555a80f3f177ee63798c44303b342a8a037b2cc
Flademir <fladermorais@hotmail.com>
2022-05-10 20:57:30 -0300

    Ajustando o menu superior e menu suspenso

--- resources/views/layouts/site/menu_suspenso.blade.php
+++ resources/views/layouts/site/menu_suspenso.blade.php
@@ -1,17 +1,10 @@
 <div id="cd-nav"> <a href="#0" class="cd-nav-trigger">Menu<span></span></a>
     <nav id="cd-main-nav">
         <ul>
-            <li class="active"><a href="index.html"><span>Home</span> <small>X-Sport Home Page</small> </a></li>
-            <li><a href="blog.html"><span>NEWS & BLOG</span> <small>X-Sport News</small></a></li>
-            <li ><a href="post.html"><span>Post </span> <small>X-Sport Post </small></a></li>
-            <li><a href="shop.html" class="dropdown-toggle" data-toggle="dropdown"><span>Shop<b class="caret"></b></span> <small>SEE ALL MODELS</small></a>
-                <ul>
-                    <li><a href="shop.html">Home</a></li>
-                    <li><a href="catalog.html">Catalog</a></li>
-                    <li><a href="product.html"  >Product</a></li>
-                </ul>
-            </li>
-            <li ><a href="page.html"><span>Page</span> <small>X-Sport page </small></a></li>
+            <li class="" ><a href="{{ route('home') }}">Home</a></li>
+            <li ><a href="{{ route('produtos', 'all') }}">Produtos</a></li>
+            <li><a href="{{ route('noticias') }}">Notícias</a></li>
+            <li ><a href="{{ route('home') }}/#contato">Contato</a></li>
         </ul>
     </nav>
 </div>
