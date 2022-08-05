0625eeadf86b0d27426bf26f09110aee394a5b8b
Flademir <fladermorais@hotmail.com>
2022-08-05 08:18:07 -0300

    Alternado forma de exibir o slide e implementando plugin para rotação

--- resources/views/layouts/site/banner2.blade.php
+++ resources/views/layouts/site/banner2.blade.php
@@ -1,4 +1,4 @@
-<div class="banner-inicial">
+{{-- <div class="banner-inicial">
     @foreach($banners as $banner)
     <div class="banner-imagem" style="background: url({{ asset('/storage/banners/' . $banner->imagem) }}) no-repeat top center ">
         
@@ -16,4 +16,48 @@
     </div>
     
     @endforeach
-</div>
+</div> --}}
+
+
+<div class="introducao slide">
+    @foreach($banners as $banner)
+    <a target="_blank" href="{{ $banner->link }}">
+        <img src="{{ asset('/storage/banners/' . $banner->imagem) }}" alt="Bike Brothers">
+        <h2>{!! $banner->descricao !!}</h2>
+
+        <div class="caption-text">
+            <p>BIKE BROTHERS</p>
+        </div>
+        
+    </a>
+    @endforeach
+</div> 
+
+<script>
+    function slider(sliderName, velocidade) {
+        var sliderClass = '.' + sliderName,
+            activeClass = 'active',
+            rotate = setInterval(rotateSlide, velocidade)
+
+        $(sliderClass + ' > :first').addClass(activeClass);
+        
+        $(sliderClass).hover(function() {
+            clearInterval(rotate);
+        }, function() {
+            rotate = setInterval(rotateSlide, velocidade)
+        });
+        function rotateSlide() {
+            var activeSlide = $(sliderClass + ' > .'+ activeClass),
+            nextSlide   = activeSlide.next();
+            
+            if(nextSlide.length == 0){
+                nextSlide = $(sliderClass + ' > :first');
+            }
+            
+            activeSlide.removeClass(activeClass);
+            nextSlide.addClass(activeClass);
+        }
+    }
+
+    slider('introducao', 3000);
+</script>
