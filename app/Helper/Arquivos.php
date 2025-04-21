6c4d1888dbcb6c9a9c3c77c356a14177a41a696b
Flademir Morais <fladermorais@hotmail.com>
2025-04-21 20:22:51 -0300

    [Arquivo] Criando um helper para gerenciamento dos arquivos

---
+++ app/Helper/Arquivos.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Helper;
+
+class Arquivos 
+{
+    public static function uploadArquivo($arquivo, $alias, $dir)
+    {
+        $file = $arquivo;
+        $extensao = $arquivo->getClientOriginalExtension();
+        $nomeArquivo =  $alias. "." .$extensao;
+        $path = public_path('/storage/' . $dir);
+        
+        $file->move($path, $nomeArquivo);
+        
+        if(!$file){
+            flash('Falha ao fazer o upload do Arquivo')->warning();
+            return redirect()
+            ->back()
+            ->with('error', 'Falha ao fazer upload do Arquivo')
+            ->withInput();
+        }
+        return $nomeArquivo;
+    }
+
+    public static function unlinkArquivo($arquivo, $dir)
+    {
+        $path = public_path('/storage/' . $dir);
+        $file = $arquivo;
+        $arquivo = $path.$file;
+        
+        if(file_exists($arquivo)){
+            unlink($arquivo);
+            return true;
+        }
+    }
+
+    public static function getAlias($string)
+    {
+        $string = str_replace("?", "_", $string);
+        $string = str_replace("!", "_", $string);
+        $string = str_replace(",", "_", $string);
+        $string = str_replace(' ', "_", $string);
+        $string = strtolower($string);
+        return preg_replace(array("/(á|à|ã|â|ä)/","/(Á|À|Ã|Â|Ä)/","/(é|è|ê|ë)/","/(É|È|Ê|Ë)/","/(í|ì|î|ï)/","/(Í|Ì|Î|Ï)/","/(ó|ò|õ|ô|ö)/","/(Ó|Ò|Õ|Ô|Ö)/","/(ú|ù|û|ü)/","/(Ú|Ù|Û|Ü)/","/(ñ)/","/(Ñ)/", "/(ç|Ç)/"),explode(" ","a A e E i I o O u U n N c"),$string);    
+    }
+}
