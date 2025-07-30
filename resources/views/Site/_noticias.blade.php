113666a15b330504c561f375be93bda519e68332
Flademir Morais <fladermorais@hotmail.com>
2025-07-30 10:59:17 -0300

    [Estilo] Corrigindo css de imagens para não utilizar scala

--- resources/views/Site/_noticias.blade.php
+++ resources/views/Site/_noticias.blade.php
@@ -21,7 +21,6 @@
                   </div>
                   <a class="noticias-img" href="{{ route('noticia', $noticia->alias) }}">
                     <img src="{{ asset('storage/noticias/' . $noticia->imagem) }}" class="size-blog-masonry-image-two-c" alt="{{ $noticia->nome }}" title="{{ $noticia->nome }}" />
-                    <div class="mask"></div>
                   </a>
                 </div>
                 <div class="post-info">
